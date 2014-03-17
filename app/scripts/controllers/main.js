3d46a069cb68806a056afda41fcba4ed72b5f44c
Marc Harry <mjharry1@gmail.com>
2014-03-17 15:50:06 +0000

    Added popover to basket for added new item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,7 @@ angular.module('shopfrontApp')
   .controller('MainCtrl', function ($scope, $http, $timeout, $routeParams, DataService) {
     $scope.store = DataService.store;
     $scope.cart = DataService.cart;
+    var count = $scope.cart.getTotalCount();
 
     if($routeParams.productId != null) {
     	$scope.product = $scope.store.getProduct($routeParams.productId);
@@ -13,5 +14,16 @@ angular.module('shopfrontApp')
     	var $affixElement = $('div[data-spy="affix"]');
 	    $affixElement.width($affixElement.parent().width()).addClass('affix');
     });
-  });
+
+    $scope.giveProductAlert = function() {
+        if ($scope.cart.getTotalCount() > count) {
+            console.log('added');
+            $("#basket").popover({ content: 'Item added to basket.', placement: 'bottom', trigger: 'manual' });
+            $("#basket").popover('show');
+            $timeout(function() {
+                $("#basket").popover('destroy');
+            }, 2000);
+        }
+    };
+});
 
